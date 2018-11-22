0b064c0e8618a76648253c4f98d5e0551c283a53
MehtiKendrick <mk1243@york.ac.uk>
2018-11-22 15:11:53 +0000

    Fixed some type errors (string to String) and added a constructor.

---
+++ York Pirates/core/src/com/rear_admirals/york_pirates/College.java
@@ -0,0 +1,13 @@
+package com.rear_admirals.york_pirates;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.HashMap;
+
+public class College extends Island{
+
+    Public String Name;
+    Public College Ally;
+    Public College Enemy;
+
+}

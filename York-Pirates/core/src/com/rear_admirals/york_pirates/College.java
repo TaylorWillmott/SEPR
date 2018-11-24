9013f14447f9ac4fe68115ba258d690f0127c9c4
Taylor W <43759113+TheRandomnessGuy@users.noreply.github.com>
2018-11-24 18:21:27 +0000

    Added constructors, getters and setters
    Added a couple of constructors as well as the necessary getters and setters for the College class. Also set name to final as it shouldn't need to be changed once initialised.

--- York-Pirates/core/src/com/rear_admirals/york_pirates/College.java
+++ York-Pirates/core/src/com/rear_admirals/york_pirates/College.java
@@ -2,8 +2,25 @@ package com.rear_admirals.york_pirates;
 
 public class College {
 
-    public String name;
+    public final String name;
     public College ally;
     public College enemy;
+
+    public College(String name, College ally, College enemy) {
+    	this.name = name;
+    	this.ally = ally;
+    	this.enemy = enemy;
+    }
+
+    public College(String name) {
+    	this(name, null, null);
+    }
+
+    public String getName() { return name; }
+    public College getAlly() { return ally; }
+    public College getEnemy() { return enemy; }
+
+    public void setAlly(College ally) { this.ally = ally; }
+    public void setEnemy(College enemy) { this.enemy = enemy; }
 
 }

458f7a76b7b9d41984cd6a035d19b300059e03b6
Taylor W <43759113+TheRandomnessGuy@users.noreply.github.com>
2018-11-24 17:47:05 +0000

    Added Grapeshot attack
    Added a grapeshot attack. Currently fires 4 half-accuracy shots and deals half as much damage per successful hit.

---
+++ York-Pirates/core/src/com/rear_admirals/york_pirates/Attacks/GrapeShot.java
@@ -0,0 +1,24 @@
+package com.rear_admirals.york_pirates.Attacks;
+
+import com.rear_admirals.york_pirates.Ship;
+
+public class GrapeShot extends Attack {
+	private String name = "Grape Shot";
+	private String desc = "Fire a bundle of smaller cannonballs at the enemy.";
+
+	@Override
+	public boolean doAttack(Ship attacker, Ship defender) {
+		boolean toReturn = false; // Tracks if any shots have hit.
+
+		for (int i = 0; i < 4; i++) { // Fires 4 shots.
+			if (doesHit(attacker.getAccuracy(), 5, 100)) { // Multiplier is halved, so each shot is half as likely to hit.
+				defender.damage(attacker.getAttack() / 2); // Landed shots do half as much damage as a regular shot.
+				toReturn = true;
+			}
+		}
+		return toReturn;
+	}
+
+	public static Attack attackGrape = new GrapeShot();
+
+}

7c4a64587b08a3c7a769ae4ee2426c1fbacc7a1b
Taylor W <43759113+TheRandomnessGuy@users.noreply.github.com>
2018-11-30 11:09:50 +0000

    Changed background colour to use float values. Background is now blue.

--- York-Pirates/core/src/com/rear_admirals/york_pirates/Combat/ShipCombat.java
+++ York-Pirates/core/src/com/rear_admirals/york_pirates/Combat/ShipCombat.java
@@ -65,7 +65,7 @@ public class ShipCombat implements Screen {
 
 	@Override
 	public void render (float delta) {
-	    Gdx.gl.glClearColor(1,1,1,1);
+	    Gdx.gl.glClearColor((float) 0.26,(float) 0.52,(float) 0.95,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
         stage.draw();

40b58ee7aa5d9697f61cc609bd343b36940a085c
Taylor W <43759113+TheRandomnessGuy@users.noreply.github.com>
2019-02-15 13:10:40 +0000

    Cleared obsolete code

--- pi-rates/core/src/display/MenuScreen.java
+++ pi-rates/core/src/display/MenuScreen.java
@@ -12,7 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.utils.compression.lzma.Base;
 import game_manager.GameManager;
 
 public class MenuScreen extends BaseScreen{

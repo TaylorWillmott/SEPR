f583b0f4a510c5efb51b3478483ad40577863392
AlexHodder <ahw519@york.ac.uk>
2019-04-30 00:34:46 +0100

    commit to pull

--- York-Pirates/desktop/src/com/rear_admirals/york_pirates/desktop/DesktopLauncher.java
+++ York-Pirates/desktop/src/com/rear_admirals/york_pirates/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.rear_admirals.york_pirates.PirateGame;
 
-public class DesktopLauncher {
+class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "York Pirates!";
